9e8dde6875bb90714f26e2cb8c497e02b1b03a59
Steffen <schultze.steffen@googlemail.com>
2018-12-30 10:56:03 +0000

    misc fixes

--- bottle_app.py
+++ bottle_app.py
@@ -1,4 +1,4 @@
-from bottle import route, static_file, run, Bottle
+from bottle import route, static_file, run, Bottle, response
 import sentence
 import mandala
 
@@ -16,23 +16,25 @@ def top():
 def bla(nsym = None, seed = None, colorindex = None, outline = 0):
     if nsym is None:
         nsym = 5
-    nsym = min(max(nsym, 2), 16)
+    nsym = min(max(nsym, 1), 16)
     if colorindex is not None:
         colorindex = min(max(colorindex, 0), 99)
-    return mandala.getMandalaSVG(nsym, True, seed, colorindex)
+    response.set_header('Content-Type', 'image/svg+xml')
+    return '<?xml version="1.0" encoding="utf-8" ?>\n' + mandala.getMandalaSVG(nsym, True, seed, colorindex)
 
         
-@app.route('/mandala2')
-@app.route('/mandala2/<nsym:int>')
-@app.route('/mandala2/<nsym:int>/<seed:int>')
-@app.route('/mandala2/<nsym:int>/<seed:int>/<colorindex:int>')
+@app.route('/mandalar')
+@app.route('/mandalar/<nsym:int>')
+@app.route('/mandalar/<nsym:int>/<seed:int>')
+@app.route('/mandalar/<nsym:int>/<seed:int>/<colorindex:int>')
 def bla(nsym = None, seed = None, colorindex = None, outline = 0):
     if nsym is None:
         nsym = 5
-    nsym = min(max(nsym, 2), 16)
+    nsym = min(max(nsym, 1), 16)
     if colorindex is not None:
         colorindex = min(max(colorindex, 0), 99)
-    return mandala.getMandalaSVG(nsym, False, seed, colorindex)
+    response.set_header('Content-Type', 'image/svg+xml')
+    return '<?xml version="1.0" encoding="utf-8" ?>\n' +  mandala.getMandalaSVG(nsym, False, seed, colorindex)
 
 
 run(app, host="0.0.0.0", port = 10001, debug = True)
